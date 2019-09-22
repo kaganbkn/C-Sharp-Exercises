f5932a439919c3b2c001bece1344e7ac5fb9e20a
kagan <kaganb78@gmail.com>
2019-09-22 22:27:57 +0300

    Initial Commit

---
+++ Exercises/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrays
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Animals = new string[] { "cat", "dog", "penguin" };
+            string[] Animals2 = { "crocodile","tiger" };
+            var numbers = new int[] { 1, 2, 3, 4 };
+            WriteNumber(numbers);
+
+
+
+            Console.ReadLine();
+        }
+
+        public static void WriteNumber(int[] array)
+        {
+            foreach(var number in array)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
