bacd6b9d46e2e99dde638e2be958e143dd526aa4
Kağan Beken <kagan.beken@sestek.com>
2019-09-24 18:42:03 +0300

    updated async

---
+++ Async-Await/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Async_Await
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteA();
+            WriteB();
+            Console.ReadLine();
+        }
+        public static async Task WriteA()
+        {
+            await Task.Run(() =>
+            {
+                for(var i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("A");
+                }
+            });
+        }
+        public static void WriteB()
+        {            
+            for(var i = 0; i < 25; i++)
+            {
+                Console.WriteLine("B");
+            }
+        }
+    }
+}
