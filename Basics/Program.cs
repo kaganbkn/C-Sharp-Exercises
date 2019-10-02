67e8987f8c9aa0d4d3004ae46a7f5a6556ebddd9
kagan <kaganb78@gmail.com>
2019-10-02 22:57:16 +0300

    added basics

---
+++ Basics/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in args)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();
+            //var cChar = Console.ReadKey().KeyChar;
+
+            // Assign Casdonsole.Title property to string returned by ReadLine.
+            Console.Title = Console.ReadLine();
+
+            try
+            {
+                var a = Convert.ToInt32(Console.ReadLine());
+                var temp = 30 / a;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Source);
+            }
+            finally
+            {
+                Console.WriteLine("From Finally");
+            }
+
+        }
+    }
+}   
