e8cf2ac65dca24fe44cb7c00beb6dbb46c1c48d0
Kağan Beken <kagan.beken@sestek.com>
2019-10-11 19:31:42 +0300

    added kvpairs

---
+++ src/Collections/KVPair/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace KVPair
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args=new [] { "asd"};
+
+            foreach (var arg in args)
+            {
+                Console.WriteLine($"Args : {arg}");
+            }
+
+
+
+
+
+
+
+
+
+
+            var number = new List<KeyValuePair<string,int>>();
+            var obj=new Program();
+            //obj.Write();
+
+
+            Console.Read();
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("ads");
+        }
+    }
+}
---
+++ src/Commons/Polymorphism/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        static int Sum(int a, int b,int c)
+        {
+            return a + b + c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Sum(1,2));
+            Console.WriteLine(Sum(1, 2,3));
+
+            ////////
+            ///
+            
+            A obj=new A();
+            obj.Test();
+            B obj1=new B();
+            obj1.Test();
+
+
+            Console.Read();
+        }
+    }
+
+    public class A
+    {
+        public virtual void Test()
+        {
+            Console.WriteLine("A.Test");
+        }
+    }
+
+    public class B : A
+    {
+        public override void Test()
+        {
+            Console.WriteLine("B.Test");
+        }
+    }
+}
